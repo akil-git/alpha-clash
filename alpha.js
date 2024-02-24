972f11a98d4f8f13244d99c4497950b7c49a4f9c
akil <sayemakil456@gmail.com>
2024-02-25 02:47:31 +0600

    more functions added

--- alpha.js
+++ alpha.js
@@ -1,30 +1,43 @@
-
 function continueGame() {
     const alphabetRand = getRandomAlphabet()
-   
     const currentAlphabetElement = document.getElementById('current-alphabet')
     currentAlphabetElement.innerText = alphabetRand
     setBackgroundColor(alphabetRand);
 
 }
-
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();
 }
 // for pressing individual key in the keyboard 
-
 function handleKeyboardKeyUpEvent(event) {
     const playerPressed = event.key;
     console.log( 'player pressed', playerPressed)
 
+    // EXPECTED PRESS
+
+    const currentAlpha = document.getElementById('current-alphabet')
+    const presentAlphabet = currentAlpha.innerText
+    const expectedAlphabet = presentAlphabet.toLowerCase()
+    
+    console.log(playerPressed, expectedAlphabet);
+
+    //  CHECKING matched 
+
+    if (playerPressed == expectedAlphabet) {
+        console.log("you have gained a point");
+        continueGame()
+        RemoveBackgroundColor(expectedAlphabet)
+        
+    }
+    else{
+        console.log('you lose a life');
+    }
+
 
     
-    // key player is expected to press
-    const currentAlphabetElement = document.getElementById('current-alphabet');
-    const currentAlphabet = currentAlphabetElement.innerText;
-    const expectedAlphabet = currentAlphabet.toLowerCase();
+   
 
     
 }
--- utility-akil.js
+++ utility-akil.js
@@ -12,6 +12,10 @@ function setBackgroundColor(elementId){
     const element = document.getElementById(elementId)
     element.classList.add('bg-orange-400')
 }
+function RemoveBackgroundColor(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400')
+}
 
 function getRandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
